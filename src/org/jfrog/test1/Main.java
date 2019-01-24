a5f11a1ae2b308efbfe76d0989f162d343cf9e52
David Pinhas <davidp@jfrog.com>
2019-01-24 09:44:06 +0200

    Initial Commit

---
+++ src/org/jfrog/test1/Main.java
@@ -0,0 +1,10 @@
+package org.jfrog.test1;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int num1 = 2;
+        int num2 = 3;
+        if (num2 >= num1) System.out.println("Success");
+    }
+}
